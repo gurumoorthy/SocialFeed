f21c525b0f1498a35480763c87f00eb5f7ab4eac
Gurumoorthy Arumugam <gurumoorthy.a@etihadwe.ae>
2025-04-27 20:29:05 +0400

    Initial Commit

---
+++ SocialFeed/SocialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialFeedApp.swift
+//  SocialFeed
+//
+//  Created by FEWA on 27/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SocialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SocialFeedTests/SocialFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  SocialFeedTests.swift
+//  SocialFeedTests
+//
+//  Created by FEWA on 27/04/2025.
+//
+
+import Testing
+@testable import SocialFeed
+
+struct SocialFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
